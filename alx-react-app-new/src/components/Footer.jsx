ef6389baee94f9720d5553450e702234bbac894f
KojoBolt <kojobolt@gmail.com>
2025-02-19 14:49:04 +0000

    added react styles

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footert (){
+    return (
+        <footer style={{backgroundColor: "black", color: "white"}}>
+             <p>© 2023 City Lovers</p>
+        </footer>
+    );
+}
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,7 @@
+export default function MainContent () {
+    return (
+    <main style={{border: '1px solid green', borderRadius: '6px',}}>
+        <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+    );
+}
