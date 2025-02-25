dc47431ae2067f3db0959840d0cb1bb63ddb4b96
KojoBolt <kojobolt@gmail.com>
2025-02-25 11:00:46 +0000

    added react

--- my-company/src/Components/Navbar.jsx
+++ my-company/src/Components/Navbar.jsx
@@ -2,13 +2,14 @@ import { Link } from "react-router-dom";
 
 function NavBar () {
     return (
-        <>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/services">Services</Link>
-        </>
-        
+        <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/services">Services</Link></li>
+        </ul>
+      </nav>
     )
 }
 
