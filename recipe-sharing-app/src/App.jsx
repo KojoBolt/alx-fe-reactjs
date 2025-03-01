9e5be70e8a092b4765bcd06bf82b7909dcb07bcc
KojoBolt <kojobolt@gmail.com>
2025-03-01 19:16:19 +0000

    recipe page

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<RecipeList />} />
+        <Route path="/addRecipeForm/" element={<AddRecipeForm />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App; 
