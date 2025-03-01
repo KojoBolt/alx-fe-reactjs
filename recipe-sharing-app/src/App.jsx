384cfa770a422c0d12baf5aacd7ec03b64328fe6
KojoBolt <kojobolt@gmail.com>
2025-03-01 19:29:25 +0000

    update recipe page

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,15 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
-import recipeStore from './components/recipeStore';
-
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<RecipeList />} />
         <Route path="/addRecipeForm/" element={<AddRecipeForm />} />
-        <Route path="/recipestore/" element={<recipeStore />} />
       </Routes>
     </Router>
   )
