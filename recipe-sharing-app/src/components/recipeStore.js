d9e45c0ffcb39e6ef057ef14b401a8e949846ea1
KojoBolt <kojobolt@gmail.com>
2025-03-02 02:24:51 +0000

    update recipe page

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -4,6 +4,10 @@ import create from 'zustand'
 const useRecipeStore = create(set => ({
   recipes: [ { id: 1, title: 'Pasta', description: 'Delicious Italian pasta' },
     { id: 2, title: 'Pizza', description: 'Homemade pepperoni pizza' }],
+
+    searchTerm: "",
+    filteredRecipes: [],
+
     addRecipe: (newRecipe) =>
       set((state) => ({
         recipes: [...state.recipes, { ...newRecipe, id: state.recipes.length + 1 }],
@@ -21,6 +25,20 @@ const useRecipeStore = create(set => ({
       set((state) => ({
         recipes: state.recipes.filter((recipe) => recipe.id !== id),
       })),
+
+      setSearchTerm: (term) => {
+        set({ searchTerm: term });
+        get().filterRecipes(); 
+      },
+
+      filterRecipes: () => {
+        const { recipes, searchTerm } = get();
+        const filtered = recipes.filter((recipe) =>
+          recipe.title.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        set({ filteredRecipes: filtered });
+      },
+    
 }));
 
 export default useRecipeStore;
