4385a7ecb33a3baa75c0573f1ec4fb1bb60aa14f
KojoBolt <kojobolt@gmail.com>
2025-03-02 02:30:13 +0000

    update recipe page

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,19 +1,23 @@
-  // RecipeList component
-  import { useRecipeStore } from './recipeStore';
+import React from 'react';
+import { useRecipeStore } from './recipeStore';
 
-  const RecipeList = () => {
-    const recipes = useRecipeStore(state => state.recipes);
-
+const RecipeList = () => {
+    const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
+  
     return (
       <div>
-        {recipes.map(recipe => (
-          <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
-            <p>{recipe.description}</p>
-          </div>
-        ))}
+        {filteredRecipes.length > 0 ? (
+          filteredRecipes.map((recipe) => (
+            <div key={recipe.id} className="p-4 border-b">
+              <h2 className="text-xl font-bold">{recipe.title}</h2>
+              <p>{recipe.description}</p>
+            </div>
+          ))
+        ) : (
+          <p>No recipes found.</p>
+        )}
       </div>
     );
   };
-
-  export default RecipeList; 
+  
+  export default RecipeList;
