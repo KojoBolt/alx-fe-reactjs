799e0bed15a79a012aadf0038dab21d94bdead78
KojoBolt <kojobolt@gmail.com>
2025-03-02 01:55:23 +0000

    update recipe page

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails'
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<RecipeList />} />
         <Route path="/addRecipeForm/" element={<AddRecipeForm />} />
+        <Route path="/RecipeDetails/" element={<RecipeDetails/>} />
       </Routes>
     </Router>
   )
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,9 +2,25 @@ import create from 'zustand'
 
 
 const useRecipeStore = create(set => ({
-  recipes: [],
-  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes })
+  recipes: [ { id: 1, title: 'Pasta', description: 'Delicious Italian pasta' },
+    { id: 2, title: 'Pizza', description: 'Homemade pepperoni pizza' }],
+    addRecipe: (newRecipe) =>
+      set((state) => ({
+        recipes: [...state.recipes, { ...newRecipe, id: state.recipes.length + 1 }],
+      })),
+  
+    updateRecipe: (id, updatedRecipe) =>
+      set((state) => ({
+        recipes: state.recipes.map((recipe) =>
+          recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
+        ),
+      })),
+  
+   
+    deleteRecipe: (id) =>
+      set((state) => ({
+        recipes: state.recipes.filter((recipe) => recipe.id !== id),
+      })),
 }));
 
 export default useRecipeStore;
