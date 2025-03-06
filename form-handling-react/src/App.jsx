37c8f7272ddc76a84ed34f95e3708cfc486b347f
KojoBolt <kojobolt@gmail.com>
2025-03-06 10:44:02 +0000

    react formix file extenstion change to js

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,5 +1,5 @@
 import RegistrationForm from "./components/RegistrationForm.jsx";
-import FormikForm from "./components/FormikForm.js";
+import FormikForm from "./components/formikForm.js";
 import "./index.css"
 
 function App() {
