7377d57215d390cab7b3e896497b7e95fb67011c
KojoBolt <kojobolt@gmail.com>
2025-03-07 18:36:29 +0000

    advance react router

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Profile from "./components/Profile.jsx";
 import ProfileDetails from "./components/ProfileDetails.jsx"
 import ProfileSettings from "./components/ProfileSettings.jsx"
@@ -6,13 +6,13 @@ import ProfileSettings from "./components/ProfileSettings.jsx"
  function App() {
   return (
     <div>
+      <BrowserRouter>
       <Routes>
         <Route path="/" element={<Profile.jsx/>}/>
         <Route path="/ProfileDetails" element={<ProfileDetails.jsx/>}/>
         <Route path="ProfileSettings" element={<ProfileSettings.jsx/>}/>
-
-
       </Routes>
+      </BrowserRouter>
     </div>
   )
 }
