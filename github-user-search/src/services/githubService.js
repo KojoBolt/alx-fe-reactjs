d380465181d59a983d62fa56161d5a844860dc4c
KojoBolt <kojobolt@gmail.com>
2025-04-06 14:00:42 +0000

    added more search components

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,16 +1,8 @@
 import axios from 'axios';
 
-export const fetchUsers = async (username, location, minRepos, page = 1) => {
-  const query = [];
-
-  if (username) query.push(`${username} in:login`);
-  if (location) query.push(`location:${location}`);
-  if (minRepos) query.push(`repos:>=${minRepos}`);
-
-  const searchQuery = query.join('+');
-
+export const fetchUserData = async (username) => {
   try {
-    const response = await axios.get(`https://api.github.com/search/users?q=${searchQuery}&page=${page}&per_page=10`);
+    const response = await axios.get(`https://api.github.com/users/${username}`);
     return response.data;
   } catch (error) {
     throw error;
